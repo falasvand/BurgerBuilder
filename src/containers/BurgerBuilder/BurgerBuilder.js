9c449058cb7eba6dce7f258e1ef160bd02ba862f
FaridAlasvand <farid.alasvand@mvctech.ca>
2018-08-21 13:39:40 -0400

    Added Burger and BurgetIngredient components/styling

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Auxx from '../../hoc/Auxx';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component{
+    render() {
+        return(
+            <Auxx>
+                <Burger />
+                <div>Build Controls</div>
+            </Auxx>
+        );
+    }
+}
+
+export default BurgerBuilder;
