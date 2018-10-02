a64b0eaf6788acd275d0515b9f708b601c85be89
FaridAlasvand <farid.alasvand@mvctech.ca>
2018-10-02 16:26:47 -0400

    Adds routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -104,40 +104,41 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        this.setState({
-            loading: true
-        });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer : {
-                name: 'Test User',
-                address: {
-                    street: '111 Yonge St',
-                    zipCode: '12345',
-                    country: 'Canada'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        };
-        axios.post('/orders.json', order)
-            .then(
-                response => {
-                    this.setState({
-                        loading: false,
-                        purchasing: false
-                    });
-                }
-            )
-            .catch(
-                error => {
-                    this.setState({
-                        loading: false,
-                        purchasing: false
-                    });
-                }
-            );
+        // this.setState({
+        //     loading: true
+        // });
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer : {
+        //         name: 'Test User',
+        //         address: {
+        //             street: '111 Yonge St',
+        //             zipCode: '12345',
+        //             country: 'Canada'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // };
+        // axios.post('/orders.json', order)
+        //     .then(
+        //         response => {
+        //             this.setState({
+        //                 loading: false,
+        //                 purchasing: false
+        //             });
+        //         }
+        //     )
+        //     .catch(
+        //         error => {
+        //             this.setState({
+        //                 loading: false,
+        //                 purchasing: false
+        //             });
+        //         }
+        //     );
+        this.props.history.push('/checkout');
     };
 
     render() {
