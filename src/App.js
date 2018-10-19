f15be9b23828d4169d58fbd1b0636f84a2edabab
falasvand <farid.alasvand@gmail.com>
2018-10-19 19:11:27 -0400

    routing bug fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Route, withRouter, Redirect } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -17,28 +17,30 @@ class App extends Component {
 
     render() {
         let routes = (
-            <Layout>
+            <Switch>
                 <Route path="/auth" component={Auth} />
                 <Route path="/" exact component={BurgerBuilder} />
                 <Redirect to="/" />
-            </Layout>
+            </Switch>
         );
 
         if (this.props.isAuthenticated) {
             routes = (
-                <Layout>
+                <Switch>
                     <Route path="/checkout" component={Checkout} />
                     <Route path="/orders" component={Orders} />
                     <Route path="/logout" component={Logout} />
                     <Route path="/" exact component={BurgerBuilder} />
                     <Redirect to="/" />
-                </Layout>
+                </Switch>
             );
         }
 
         return (
             <div>
-                {routes}
+                <Layout>
+                    {routes}
+                </Layout>
             </div>
         );
     }
