b24c746278d4806f99f952de8c3c5ad791eb7138
falasvand <farid.alasvand@gmail.com>
2018-10-20 11:17:48 -0400

    minor improvements and fixes

--- src/containers/auth/Auth.js
+++ src/containers/auth/Auth.js
@@ -5,6 +5,7 @@ import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css';
+import {checkValidity} from '../../shared/validation';
 import * as actions from '../../store/actions/index';
 
 class Auth extends Component {
@@ -48,27 +49,13 @@ class Auth extends Component {
         }
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-        if (rules.required) {
-            isValid = (value.trim() !== '') && isValid;
-        }
-        if (rules.minLength) {
-            isValid = (value.length >= rules.minLength) && isValid;
-        }
-        if (rules.maxLength) {
-            isValid = (value.length <= rules.maxLength) && isValid;
-        }
-        return isValid;
-    }
-
     inputChangedHandler = (event, controlName) => {
         const updatedControls = {
             ...this.state.controls,
             [controlName]: {
                 ...this.state.controls[controlName],
                 value: event.target.value,
-                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                valid: checkValidity(event.target.value, this.state.controls[controlName].validation),
                 touched: true
             }
         };
